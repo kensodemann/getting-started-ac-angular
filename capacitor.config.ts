c6dd88dc446a422fb1c10bd39a7cb7f5110b59ff
Ken Sodemann <ken@ionic.io>
2021-08-31 10:03:30 -0500

    chore(): make it pretty

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.gettingstartedacangular',
   appName: 'getting-started-ac-angular',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
